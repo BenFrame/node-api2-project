4d3e82897f0e6966f05b49bb8cd4d012258fab40
Ben Frame <brewerytattooer@gmail.com>
2024-01-16 12:59:56 -0500

    passed all test

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -99,7 +99,9 @@ router.put('/:id', (req, res) => {
             }
         })
         .then(post => {
-            res.json(post)
+            if(post){
+                res.json(post)
+            }
         })
         .catch(err => {
             res.status(500).json({
@@ -110,8 +112,24 @@ router.put('/:id', (req, res) => {
         })
     }
 })
-router.get('/:id/messages', (req, res) => {
-
+router.get('/:id/messages', async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id)
+        if(!post){
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+        } else {
+            const messages = await Post.findPostComments(req.params.id)
+            res.json(messages)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The comments information could not be retrieved",
+            err: err.message, 
+            stack: err.stack,
+    })  
+    }
 })
 
 module.exports = router
